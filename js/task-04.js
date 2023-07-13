016db5d42fd9f365da8deeb5abad085f778dc675
zadumka <90329284+zadumka@users.noreply.github.com>
2023-07-13 18:18:45 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,18 @@
-//"use strict"
-//const refs = {
-//    output: document.querySelector('#value'),
-//    incrementButton: document.querySelector('[data-action="increment"]'),
-//    decrementButton: document.querySelector('[data-action="decrement"]'),
-//};
+"use strict"
+const refs = {
+    output: document.querySelector('#value'),
+    incrementButton: document.querySelector('[data-action="increment"]'),
+    decrementButton: document.querySelector('[data-action="decrement"]'),
+};
 
-//let counterValue = 0;
+let counterValue = 0;
 
-//refs.incrementButton.addEventListener('click', () => {
-//    counterValue += 1;
-//    refs.output.textContent = counterValue;
-//});
+refs.incrementButton.addEventListener('click', () => {
+    counterValue += 1;
+    refs.output.textContent = counterValue;
+});
 
-//refs.decrementButton.addEventListener('click', () => {
-//    counterValue -= 1;
-//    refs.output.textContent = counterValue;
-//});
+refs.decrementButton.addEventListener('click', () => {
+    counterValue -= 1;
+    refs.output.textContent = counterValue;
+});
